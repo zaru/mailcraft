0468393efcf55f8e645aae5c2b1bade8cc0f23cc
zaru@sakuraba <zarutofu@gmail.com>
2017-01-11 01:25:22 +0900

    implemented to forward incoming mail

--- mini-smtp.rb
+++ mini-smtp.rb
@@ -1,7 +1,23 @@
+require 'mail'
 require 'mini-smtp-server'
+require './telnet-send-email.rb'
 
 class StdoutSmtpServer < MiniSmtpServer
     def new_message_event(message_hash)
+        mail = Mail.new(message_hash[:data])
+        from = mail.to.first
+        to = "" #TODO: fetch register email
+        subject = mail.subject
+
+        if mail.multipart?
+          message = mail.parts.first.body.decoded
+        else
+          message = mail.body.decoded
+        end
+
+        telnet = TelnetSendEmail.new(to: to, from: from, subject: subject, message: message)
+        telnet.send_email
+
         file = File.open('/tmp/smtp-log','a')
         file.puts message_hash
         file.close
